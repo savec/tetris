7412249340102abbdc9b719782c09d13a3c075e7
Gleb Chashchukhin <gleb.chashchuhin@konsec.com>
2020-06-14 15:07:42 +0200

    added rotations

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use std::ops::Deref;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Point (i8, i8);
-enum Type {
+enum TetrominoType {
     I,
     L,
     J,
@@ -21,66 +21,164 @@ enum Type {
     T,
     Z
 }
+#[derive(Debug)]
+enum TetrominoAngle {
+    Degree0,
+    Degree90,
+    Degree180,
+    Degree270
+}
+
+#[derive(Debug)]
+enum TetrisEvent {
+    MoveDown,
+    MoveRight,
+    MoveLeft,
+    RotateCW,
+    RotateCCW,
+    Drop,
+    Quit
+}
 
 #[derive(Debug)]
 struct Tetromino {
-    points: HashSet<Point>,
+    points_0: HashSet<Point>,
+    points_90: HashSet<Point>,
+    points_180: HashSet<Point>,
+    points_270: HashSet<Point>,
     origin: Point,
-    color: Box<dyn Color>
+    color: Box<dyn Color>,
+    angle: TetrominoAngle
 }
 
 impl Tetromino {
-    fn new(t: Type) -> Self {
+    fn new(t: TetrominoType) -> Self {
         match t {
-            Type::I => Tetromino {
-                points: [Point(0, 1), Point(1, 1), Point(2, 1), Point(3, 1)].
+            TetrominoType::I => Tetromino {
+                points_0: [Point(0, 1), Point(1, 1), Point(2, 1), Point(3, 1)].
+                    iter().cloned().collect(),
+                points_90: [Point(2, 0), Point(2, 1), Point(2, 2), Point(3, 2)].
+                    iter().cloned().collect(),
+                points_180: [Point(0, 2), Point(1, 2), Point(2, 2), Point(3, 2)].
+                    iter().cloned().collect(),
+                points_270: [Point(1, 0), Point(1, 1), Point(1, 2), Point(1, 3)].
                     iter().cloned().collect(),
                 origin: Point(2, 0),
-                color: Box::new(color::Cyan)
+                color: Box::new(color::Cyan),
+                angle: TetrominoAngle::Degree0,
             },
-            Type::J => Tetromino {
-                points: [Point(0, 0), Point(0, 1), Point(1, 1), Point(2, 1)].
+            TetrominoType::J => Tetromino {
+                points_0: [Point(0, 0), Point(0, 1), Point(1, 1), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_90: [Point(1, 0), Point(2, 0), Point(1, 1), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_180: [Point(0, 1), Point(1, 1), Point(2, 1), Point(2, 2)].
+                    iter().cloned().collect(),
+                points_270: [Point(0, 2), Point(1, 0), Point(1, 1), Point(1, 2)].
                     iter().cloned().collect(),
                 origin: Point(2, 0),
-                color: Box::new(color::Blue)
+                color: Box::new(color::Blue),
+                angle: TetrominoAngle::Degree0,
             },
-            Type::L => Tetromino {
-                points: [Point(0, 1), Point(1, 1), Point(2, 1), Point(2, 0)].
+            TetrominoType::L => Tetromino {
+                points_0: [Point(0, 1), Point(1, 1), Point(2, 1), Point(2, 0)].
+                    iter().cloned().collect(),
+                points_90: [Point(1, 0), Point(1, 1), Point(1, 2), Point(2, 2)].
+                    iter().cloned().collect(),
+                points_180: [Point(0, 1), Point(1, 1), Point(2, 1), Point(0, 2)].
+                    iter().cloned().collect(),
+                points_270: [Point(0, 0), Point(1, 0), Point(1, 1), Point(1, 2)].
                     iter().cloned().collect(),
                 origin: Point(2, 0),
-                color: Box::new(color::Yellow)
+                color: Box::new(color::Yellow),
+                angle: TetrominoAngle::Degree0,
             },
-            Type::O => Tetromino {
-                points: [Point(1, 0), Point(1, 1), Point(2, 0), Point(2, 1)].
+            TetrominoType::O => Tetromino {
+                points_0: [Point(1, 0), Point(1, 1), Point(2, 0), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_90: [Point(1, 0), Point(2, 1), Point(1, 0), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_180: [Point(1, 0), Point(2, 1), Point(1, 0), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_270: [Point(1, 0), Point(2, 1), Point(1, 0), Point(2, 1)].
                     iter().cloned().collect(),
                 origin: Point(2, 0),
-                color: Box::new(color::LightYellow)
+                color: Box::new(color::LightYellow),
+                angle: TetrominoAngle::Degree0,
+
             },
-            Type::S => Tetromino {
-                points: [Point(0, 1), Point(1, 0), Point(1, 1), Point(2, 0)].
+            TetrominoType::S => Tetromino {
+                points_0: [Point(0, 1), Point(1, 0), Point(1, 1), Point(2, 0)].
+                    iter().cloned().collect(),
+                points_90: [Point(1, 0), Point(1, 1), Point(2, 1), Point(2, 2)].
+                    iter().cloned().collect(),
+                points_180: [Point(0, 2), Point(1, 1), Point(1, 2), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_270: [Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 2)].
                     iter().cloned().collect(),
                 origin: Point(2, 0),
-                color: Box::new(color::Green)
+                color: Box::new(color::Green),
+                angle: TetrominoAngle::Degree0,
+
             },
-            Type::T => Tetromino {
-                points: [Point(1, 0), Point(0, 1), Point(1, 1), Point(2, 1)].
+            TetrominoType::T => Tetromino {
+                points_0: [Point(1, 0), Point(0, 1), Point(1, 1), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_90: [Point(1, 0), Point(1, 1), Point(1, 2), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_180: [Point(0, 1), Point(1, 1), Point(1, 2), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_270: [Point(0, 1), Point(1, 0), Point(1, 1), Point(1, 2)].
                     iter().cloned().collect(),
                 origin: Point(3, 0),
-                color: Box::new(color::Magenta)
+                color: Box::new(color::Magenta),
+                angle: TetrominoAngle::Degree0,
             },
-            Type::Z => Tetromino {
-                points: [Point(0, 0), Point(1, 0), Point(1, 1), Point(2, 1)].
+            TetrominoType::Z => Tetromino {
+                points_0: [Point(0, 0), Point(1, 0), Point(1, 1), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_90: [Point(1, 1), Point(1, 2), Point(2, 0), Point(2, 1)].
+                    iter().cloned().collect(),
+                points_180: [Point(0, 1), Point(1, 1), Point(1, 2), Point(2, 2)].
+                    iter().cloned().collect(),
+                points_270: [Point(0, 1), Point(0, 2), Point(1, 0), Point(1, 1)].
                     iter().cloned().collect(),
                 origin: Point(2, 0),
-                color: Box::new(color::Red)
+                color: Box::new(color::Red),
+                angle: TetrominoAngle::Degree0,
             }
         }
     }
 
     fn get_current_set(&self) -> HashSet<Point> {
-        self.points.clone().into_iter().
+        let points = match self.angle {
+            TetrominoAngle::Degree0 => self.points_0.clone(),
+            TetrominoAngle::Degree90 => self.points_90.clone(),
+            TetrominoAngle::Degree180 => self.points_180.clone(),
+            TetrominoAngle::Degree270 => self.points_270.clone(),
+        };
+
+        points.into_iter().
         map(|point| Point(point.0 + self.origin.0, point.1 + self.origin.1)).
         collect()
+    }
+
+    fn rotate_cw(&mut self) {
+        self.angle = match self.angle {
+            TetrominoAngle::Degree0 => TetrominoAngle::Degree90,
+            TetrominoAngle::Degree90 => TetrominoAngle::Degree180,
+            TetrominoAngle::Degree180 => TetrominoAngle::Degree270,
+            TetrominoAngle::Degree270 => TetrominoAngle::Degree0,
+        };
+    }
+
+    fn rotate_ccw(&mut self) {
+        self.angle = match self.angle {
+            TetrominoAngle::Degree0 => TetrominoAngle::Degree270,
+            TetrominoAngle::Degree90 => TetrominoAngle::Degree0,
+            TetrominoAngle::Degree180 => TetrominoAngle::Degree90,
+            TetrominoAngle::Degree270 => TetrominoAngle::Degree180,
+        };
     }
 }
 
@@ -132,29 +230,40 @@ fn main() {
 
     thread::spawn(move || loop {
         for c in stdin().keys() {
-            tx0.send(c.unwrap()).unwrap();
+            let event = match c.unwrap() {
+                Key::Up         => Some(TetrisEvent::RotateCCW),
+                Key::Down       => Some(TetrisEvent::RotateCW),
+                Key::Left       => Some(TetrisEvent::MoveLeft),
+                Key::Right      => Some(TetrisEvent::MoveRight),
+                Key::Ctrl('q')  => Some(TetrisEvent::Quit),
+                _ => None,
+            };
+            if let Some(event) = event {
+                tx0.send(event).unwrap();
+            }
         }
     });
 
     thread::spawn(move || loop {
         thread::sleep(Duration::from_millis(500));
-        tx1.send(Key::Down).unwrap();
+        tx1.send(TetrisEvent::MoveDown).unwrap();
     });
 
 
-    let mut tet = Tetromino::new(Type::S);
+    let mut tet = Tetromino::new(TetrominoType::S);
     let mut well = Well::new();
 
     let mut stdout = stdout().into_raw_mode().unwrap(); // move out
 
     loop {
         match rx.recv().unwrap() {
-            Key::Up => tet.origin.1 -= 1,
-            Key::Down => tet.origin.1 += 1,
-            Key::Left => tet.origin.0 -= 1,
-            Key::Right => tet.origin.0 += 1,
-            Key::Ctrl('q') => break,
-            _ => (),
+            TetrisEvent::RotateCCW  => tet.rotate_ccw(),
+            TetrisEvent::RotateCW   => tet.rotate_cw(),
+            TetrisEvent::MoveLeft   => tet.origin.0 -= 1,
+            TetrisEvent::MoveRight  => tet.origin.0 += 1,
+            TetrisEvent::MoveDown   => tet.origin.1 += 1,
+            TetrisEvent::Quit       => break,
+            _ => (), // implement drop
         }
 
         render(&mut stdout, &well, &tet);
